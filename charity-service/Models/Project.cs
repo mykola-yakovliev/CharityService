9c1851d125d8a863e55ff80311a167a469fe9f42
Nickolay Yakovliev <nickolay.yakovliev@ext.verivox.com>
2019-05-22 18:02:38 +0300

    - added domain models

---
+++ charity-service/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿namespace charity_cervice.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        public int FoundationId { get; set; }
+    }
+}
