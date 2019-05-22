2f48ec7c1b24793191afbd2800b1fbeda91cbf7a
Sergey Nestertsov <sergey.nestertsov@gmail.com>
2019-05-22 18:12:48 +0300

    changed default namespace and fixed paroject name in general

--- charity-service/Controllers/SampleDataController.cs
+++ charity-service/Controllers/SampleDataController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace charity_cervice.Controllers
+namespace CharityService.Controllers
 {
     [Route("api/[controller]")]
     public class SampleDataController : Controller
