fdd6a6c6270b365fd25155e735e0b9cc4a787099
Nickolay Yakovliev <nickolay.yakovliev@ext.verivox.com>
2019-05-23 11:55:39 +0300

    - added base `ApiController` - added api end-point for categories

---
+++ charity-service/Controllers/ApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CharityService.Controllers
+{
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    [Route("api/[controller]")]
+    public abstract class ApiController : ControllerBase
+    {
+    }
+}
