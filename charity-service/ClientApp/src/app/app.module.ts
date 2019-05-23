68748d42b6c68b13d3090f62552485c3c64d138c
shohrukh92 <shohrukh.92@gmail.com>
2019-05-23 22:31:17 +0300

    Add form controls for filters

--- charity-service/ClientApp/src/app/app.module.ts
+++ charity-service/ClientApp/src/app/app.module.ts
@@ -11,11 +11,14 @@ import { ProjectDetailsComponent } from './project-details/project-details.compo
 import { PaymentPageComponent } from './payment-page/payment-page.component';
 import { ThankYouComponent } from './thank-you/thank-you.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import {MatCardModule} from '@angular/material/card';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -30,7 +33,10 @@ import {MatIconModule} from '@angular/material/icon';
     MatChipsModule,
     MatButtonModule,
     MatIconModule,
+    MatSelectModule,
+    MatCheckboxModule,
 
+    ReactiveFormsModule,
     RouterModule.forRoot(ROUTES),
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     BrowserAnimationsModule,
--- charity-service/ClientApp/src/app/project-list/project-list.component.ts
+++ charity-service/ClientApp/src/app/project-list/project-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray, FormControl } from '@angular/forms';
 import { SampleDataClient, WeatherForecast, ProjectsClient, ProjectApiModel } from 'src/generated';
 import { Router } from '@angular/router';
 import { appRoutes } from '../app.routes';
@@ -9,14 +10,53 @@ import { appRoutes } from '../app.routes';
   styleUrls: ['./project-list.component.css']
 })
 export class ProjectListComponent implements OnInit {
+  public filterForm: FormGroup;
   public data: WeatherForecast[] = [];
   
   public projects: ProjectApiModel[] = [];
+  public foundations = [
+    {
+      id: 1,
+      name: 'Foundation 1'
+    },
+    {
+      id: 2,
+      name: 'Foundation 2'
+    },
+    {
+      id: 3,
+      name: 'Foundation 3'
+    }
+  ];
+  public categories = [
+    {
+      id: 1,
+      name: 'Category 1'
+    },
+    {
+      id: 2,
+      name: 'Category 2'
+    },
+    {
+      id: 3,
+      name: 'Category 3'
+    }
+  ];
 
-  constructor(private _projectClient: ProjectsClient, private router: Router) { }
+  constructor(private _projectClient: ProjectsClient, private router: Router, private fb: FormBuilder) { }
 
   ngOnInit() {
     this._projectClient.getProjects(null, null).subscribe((projects) => this.projects = projects);
+    this.initFilterForm();
+  }
+
+  initFilterForm() {
+    this.filterForm = this.fb.group({
+      foundations: [],
+      categories: this.fb.array([])
+    });
+    const formArray = this.filterForm.get('categories') as FormArray;
+    this.categories.forEach(c => formArray.push(new FormControl(false)));
   }
 
   goToDetails(id: number) {
