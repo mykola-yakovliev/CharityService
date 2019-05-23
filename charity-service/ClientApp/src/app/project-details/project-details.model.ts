3eca992517a1ef38c3941ffb5a2ee81a8f239e28
Alexandr010496 <Alexandr010496@gmail.com>
2019-05-23 16:00:58 +0300

    make deskription optional

--- charity-service/ClientApp/src/app/project-details/project-details.model.ts
+++ charity-service/ClientApp/src/app/project-details/project-details.model.ts
@@ -3,6 +3,6 @@ export interface ProjectDetails {
     name: string;
     foundationName: string;
     categoryNames: string[];
-    description: string;
+    description?: string;
     image: string;
 }
